2f83543ef3c692b805052908043ead25870c79ef
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-05-02 23:42:12 -0700

    first commit

---
+++ .history/js/task.class_20240502223742.js
@@ -0,0 +1,36 @@
+export class task {
+    id;
+    name;
+    description;
+    dueDate;
+    stage;
+
+    constructor(id, name = "", description = "", dueDate = "2024-01-01", stage = 0) {
+
+    }
+
+    addTaskToDropArea = () => {
+
+    }
+    updateTaskToDropArea = () => {
+        // using the querySelector to select element 
+        //  document.querySelector(`[data-task-stage-id="${this.stage}"]`);
+
+        //
+    }
+
+    changeStage = (stage) => {
+        //
+    }
+
+    // generateCardHTML is for generate a html element that include the card content.
+    generateCardHTML = () => {
+        // Return Card Element
+        // const div = document.createElement("div");
+        // div.appendChild()
+    }
+
+    editTask = () => {
+    // put this task data to form
+}
+}
